f828a2a4d8f726723995965ea4ca9c9c2565fcd6
Vasyl78 <terekh.vasyl@gmail.com>
2018-03-31 23:07:40 +0300

    Add filters for items index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,15 @@ class ItemsController < ApplicationController
   before_action :set_item, only: %i[show update destroy]
 
   def index
+    items = Item.filter(filtering_params)
     if params[:page] && params[:page][:number]
       current_page = params[:page][:number].to_i
     else
       current_page = 1
     end
 
-    items = Item.paginate(page: current_page, per_page: 5)
-    pagination = pagination(current_page)
+    items = items.paginate(page: current_page, per_page: 5)
+    pagination = pagination(current_page, items)
 
     json_response(items: items, pagination: pagination)
   end
@@ -40,15 +41,15 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.permit(:name, :user_id)
+    params.permit(:name, :user_id, :category_id, :brand, :size, :daily_price)
   end
 
   def set_item
     @item = Item.find(params[:id])
   end
 
-  def pagination(current_page)
-    total_pages = (Item.count.to_f / 5).ceil
+  def pagination(current_page, items)
+    total_pages = (items.count.to_f / 5).ceil
     next_page   = current_page < total_pages ? (current_page + 1) : nil
     prev_page   = current_page > 2 ? (current_page - 1) : nil
     {
@@ -57,5 +58,13 @@ class ItemsController < ApplicationController
       next_page_url: "#{items_path}?page[number]=#{next_page}",
       prev_page_url: "#{items_path}?page[number]=#{prev_page}"
     }
+  end
+
+  def filtering_params
+    h1 = params.permit(non_booked: [],
+                       price_range_and_day: %i[min_price max_price days]).to_h
+    h2 = params.permit(:item_city, :item_category, :item_name, :item_brand,
+                       :size_range)
+    h1.merge(h2).to_h
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@
 #
 
 class Item < ApplicationRecord
+  include Filterable
   # Relations
   belongs_to :user
   belongs_to :city
@@ -23,17 +24,36 @@ class Item < ApplicationRecord
   validates :name, presence: true
 
   # Scoupes
-  scope :in_city,          ->(title) { joins(:city).where('cities.title = ?', title) }
-  scope :from_category,    ->(name)  { joins(:category).where('categories.name LIKE (?)', "%#{name}%") }
-  scope :with_name,        ->(name)  { where('name LIKE (?)', "%#{name}%") }
-  scope :with_brand,       ->(value) { where('brand = ?', value) }
-  scope :with_size,        ->(range) { where('range IN (?)', range) }
+  scope :item_city,     ->(title) { joins(:city).where('cities.title = ?', title) }
+  scope :item_category, ->(name)  { joins(:category).where('categories.name LIKE (?)', "%#{name}%") }
+  scope :item_name,     ->(name)  { where('name LIKE (?)', "%#{name}%") }
+  scope :item_brand,    ->(value) { where('brand = ?', value) }
+
+  scope :with_size,        ->(range) { where('size IN (?)', range) }
   scope :with_daily_price, ->(range) { where('daily_price IN (?)', range) }
-  scope :non_booked,       ->(range) { joins(:bookings).where.not('(bookings.start_date IN (?)) OR (bookings.end_date IN (?))', range, range)}
+  scope :item_non_booked,  ->(range) { joins(:bookings).where.not('(bookings.start_date IN (?)) OR (bookings.end_date IN (?))', range, range)}
   scope :with_daily_price_and_days, ->(range, days) { with_daily_price(price_range_for_day(range, days))}
 
   # Callbacks
   before_validation :set_city
+
+  def self.size_range(string)
+    array = string.split('..').map(&:to_i)
+    range = array[0]..array[1]
+    with_size(range)
+  end
+
+  def self.non_booked(params)
+    array = params.map(&:to_date)
+    range = array[0]..array[1]
+    item_non_booked(range)
+  end
+
+  def self.price_range_and_day(params)
+    days = params['days'].to_i
+    range = params['min_price'].to_i..params['max_price'].to_i
+    with_daily_price_and_days(range, days)
+  end
 
   private
 
@@ -43,5 +63,5 @@ class Item < ApplicationRecord
 
   def self.price_range_for_day(range, days)
     (range.first / days)..(range.last / days)
-  end
+  end  
 end
